4e677fb6682b18220c831c351f177d9be005ecfd
achasek <chaseknieper@gmail.com>
2023-11-22 15:25:15 -0500

    began test suite

--- index.test.js
+++ index.test.js
@@ -1,2 +1,15 @@
 const request = require('supertest');
 const app = require('./src/app');
+
+// jest.mock('./models/Restaurant', () => ({ create: jest.fn() }));
+// jest.mock('./models/Restaurant', () => ({ update: jest.fn() }));
+// jest.mock('./models/Restaurant', () => ({ destroy: jest.fn() }));
+
+describe('The API for /restaurants', () => {
+    test('when we send a get request, all restaurants are returned', async () => {
+        const response = await request(app)
+            .get('/restaurants')
+
+        console.log(response.body)
+    })
+})
