7898e19d45294dab1ba8f3325445121008cee699
achasek <chaseknieper@gmail.com>
2023-11-22 16:11:13 -0500

    added test

--- index.test.js
+++ index.test.js
@@ -10,6 +10,18 @@ describe('The API for /restaurants', () => {
         const response = await request(app)
             .get('/restaurants')
 
-        console.log(response.body)
-    })
+        // console.log(response.body)
+        expect(response.status).toBe(200);
+        expect(response.body).toBeDefined();
+    });
+
+    test('when we send a get request to our base route plus a valid id params, we get a single object returned', async () => {
+        const response = await request(app)
+            .get('/restaurants/1')
+
+        // console.log(response.body)
+        expect(response.status).toBe(200)
+        expect(response.body).toBeDefined()
+        expect(Array(response.body)).toHaveLength(1)
+    });
 })
